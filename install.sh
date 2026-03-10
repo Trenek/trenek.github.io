37ba876215ab246633d5757a36e1d7b4f458cd27
Trenek <70412758+Trenek@users.noreply.github.com>
2026-03-10 04:16:49 +0100

    Clean up install.sh by removing commented lines
    Removed unnecessary installation commands and comments from install.sh.

--- install.sh
+++ install.sh
@@ -7,21 +7,3 @@ pacstrap -K /mnt base linux linux-firmware nvim gdisk refind
 
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
-
-pacman -S efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog mtools dosfstools base-devel linux-headers openssh sudo
-refind-install --usedefault "$1" --alldrivers
-mkrlconf
-
-systemctl enable NetworkManager
-
-# nvim etc/hostname
-# passwd
-# 
-# nvim boot/refind_linux.conf
-# nvim boot/EFI/BOOT/refind.conf
-
-# useradd -mG wheel trenek
-# passwd trenek
-
-# EDITOR=nvim visudo
-#     uncomment wheel
