46c4a9c8a9dd0446f73faa8262708b729c9e03d6
Trenek <70412758+Trenek@users.noreply.github.com>
2026-03-10 10:42:42 +0100

    Add power-profiles-daemon to installation script

--- later.sh
+++ later.sh
@@ -8,7 +8,8 @@ pacman -S efibootmgr networkmanager network-manager-applet wireless_tools wpa_su
           cmake ninja valgrind gnuplot `#random dev` \
           vulkan-icd-loader vulkan-headers vulkan-validation-layers vulkan-tools `#vulkan` \
           texlive `#latex` \
-          vlc vlc-plugins-all `#vlc`
+          vlc vlc-plugins-all `#vlc` \
+          power-profiles-daemon '#wiatraczki, bateria'
 
 git clone --branch 0.11 https://github.com/Trenek/nvim-config ~/.config/nvim
 curl -fsSL https://trenek.github.io/.bashrc -o temp.bashrc
