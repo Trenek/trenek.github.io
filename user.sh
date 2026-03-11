6122d4bbbd8383fac007cbb3c5d7b479b76cba3e
Trenek <70412758+Trenek@users.noreply.github.com>
2026-03-11 15:46:19 +0100

    Create user.sh

---
+++ user.sh
@@ -0,0 +1,10 @@
+timedatectl set-timezone Europe/Warsaw
+git clone --branch 0.11 https://github.com/Trenek/nvim-config ~/.config/nvim
+
+curl -fsSL https://trenek.github.io/.bashrc -o temp.bashrc
+chmod temp.bashrc
+./temp.bashrc >> ~/.bashrc
+rm temp.bashrc
+
+echo "Give Trenek Password"
+passwd
