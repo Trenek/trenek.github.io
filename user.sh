4abebc63826a899195e8c9b8659d05b558c59f4d
Trenek <70412758+Trenek@users.noreply.github.com>
2026-03-11 23:04:54 +0100

    Change command to append temp.bashrc to .bashrc

--- user.sh
+++ user.sh
@@ -3,5 +3,5 @@ git clone --branch 0.11 https://github.com/Trenek/nvim-config ~/.config/nvim
 
 curl -fsSL trenek.github.io/temp.bashrc -o temp.bashrc
 chmod 777 temp.bashrc
-./temp.bashrc >> ~/.bashrc
+cat ./temp.bashrc >> ~/.bashrc
 rm temp.bashrc
