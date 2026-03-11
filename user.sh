3026cc71a926f52207f6c11c93962f3c127faa03
Trenek <70412758+Trenek@users.noreply.github.com>
2026-03-11 21:18:19 +0100

    Fix URL for downloading temp.bashrc

--- user.sh
+++ user.sh
@@ -1,7 +1,7 @@
 timedatectl set-timezone Europe/Warsaw
 git clone --branch 0.11 https://github.com/Trenek/nvim-config ~/.config/nvim
 
-curl -fsSL https://trenek.github.io/.bashrc -o temp.bashrc
+curl -fsSL trenek.github.io/temp.bashrc -o temp.bashrc
 chmod temp.bashrc
 ./temp.bashrc >> ~/.bashrc
 rm temp.bashrc
