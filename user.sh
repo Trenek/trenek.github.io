2fe7e94a31deaeefc0215d16843794a163e67b7d
Trenek <70412758+Trenek@users.noreply.github.com>
2026-03-12 00:12:06 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,7 +1,5 @@
 timedatectl set-timezone Europe/Warsaw
 git clone --branch 0.11 https://github.com/Trenek/nvim-config ~/.config/nvim
+curl -fsSL trenek.github.io/temp.bashrc >> ~/.bashrc
 
-curl -fsSL trenek.github.io/temp.bashrc -o temp.bashrc
-chmod 777 temp.bashrc
-cat ./temp.bashrc >> ~/.bashrc
-rm temp.bashrc
+source ~/.bashrc
