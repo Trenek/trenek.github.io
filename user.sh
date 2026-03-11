057aedc40f0d0230ec861fa79979f3291ee9b85b
Trenek <70412758+Trenek@users.noreply.github.com>
2026-03-12 00:21:28 +0100

    Clone and install Google Chrome from AUR
    Add installation commands for Google Chrome.

--- user.sh
+++ user.sh
@@ -2,4 +2,8 @@ timedatectl set-timezone Europe/Warsaw
 git clone --branch 0.11 https://github.com/Trenek/nvim-config ~/.config/nvim
 curl -fsSL trenek.github.io/temp.bashrc >> ~/.bashrc
 
+git clone https://aur.archlinux.org/google-chrome
+makepkg -sicD google-chrome
+rm -rf google-chrome
+
 source ~/.bashrc
