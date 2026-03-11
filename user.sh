9de1913e01155de50b69356d16db8b00dab8b6fc
Trenek <70412758+Trenek@users.noreply.github.com>
2026-03-11 21:18:32 +0100

    Change permissions of temp.bashrc to 777

--- user.sh
+++ user.sh
@@ -2,6 +2,6 @@ timedatectl set-timezone Europe/Warsaw
 git clone --branch 0.11 https://github.com/Trenek/nvim-config ~/.config/nvim
 
 curl -fsSL trenek.github.io/temp.bashrc -o temp.bashrc
-chmod temp.bashrc
+chmod 777 temp.bashrc
 ./temp.bashrc >> ~/.bashrc
 rm temp.bashrc
