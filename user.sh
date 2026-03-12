d72dbce4eee33133789cbac634004f65f39f3ef2
Trenek <70412758+Trenek@users.noreply.github.com>
2026-03-12 16:32:57 +0100

    Update user.sh to use sudo for Google Chrome install

--- user.sh
+++ user.sh
@@ -2,8 +2,9 @@ timedatectl set-timezone Europe/Warsaw
 git clone --branch 0.11 https://github.com/Trenek/nvim-config ~/.config/nvim
 curl -fsSL trenek.github.io/temp.bashrc >> ~/.bashrc
 
-git clone https://aur.archlinux.org/google-chrome
+sudo git clone https://aur.archlinux.org/google-chrome
+sudo chmod 777 google-chrome
 makepkg -sicD google-chrome
-rm -rf google-chrome
+sudo rm -rf google-chrome
 
 source ~/.bashrc
