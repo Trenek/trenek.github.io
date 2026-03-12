b2d9320d3e5312916272b38f8c85a3b1076a9ca7
Trenek <70412758+Trenek@users.noreply.github.com>
2026-03-12 17:42:05 +0100

    Update later.sh

--- later.sh
+++ later.sh
@@ -2,7 +2,7 @@ pacman -S efibootmgr networkmanager network-manager-applet wireless_tools wpa_su
           nvidia-open nvidia-utils \
           xorg \
           lemurs `#display manager (login screen)` \
-          plasma-desktop plasma-nm plasma-pa `#desktop enviroment` \
+          plasma-desktop plasma-nm plasma-pa kscreen `#desktop enviroment` \
           spectacle `#kinda bloated` `#for screenshots`
           contour  `#terminal emulator` \
           git tree-sitter-cli unzip wget fd xclip `#for nvim config` \
