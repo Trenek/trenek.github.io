d8240a62beaf2fcee8bbdae92339f50d59177338
Trenek <70412758+Trenek@users.noreply.github.com>
2026-03-26 22:07:49 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,4 +1,4 @@
-sudo timedatectl set-timezone Europe/Warsaw
+timedatectl set-timezone Europe/Warsaw
 git clone --branch 0.11 https://github.com/Trenek/nvim-config ~/.config/nvim
 curl -fsSL trenek.github.io/temp.bashrc >> ~/.bashrc
 
