df2e167634d0357be7923f0d50f167911e771632
Trenek <70412758+Trenek@users.noreply.github.com>
2026-08-29 12:34:41 +0200

    Update later.sh

--- later.sh
+++ later.sh
@@ -27,9 +27,6 @@ pacman -S base-devel linux-headers openssh sudo \
           filelight `#disk usage viewer` \
           qemu-img libewf virtualbox virtualbox-host-dkms `#informatyka sledcza`
 
-#refind-install --usedefault "$1" --alldrivers
-#mkrlconf --force
-
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
