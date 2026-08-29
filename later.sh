5ac40f44f72de8f1196cc3517b334c34773384fd
Trenek <70412758+Trenek@users.noreply.github.com>
2026-08-29 23:34:28 +0200

    Update later.sh

--- later.sh
+++ later.sh
@@ -31,16 +31,11 @@ pacman -S base-devel linux-headers openssh sudo \
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-wget https://github.com/MrVivekRajan/Grub-Themes/releases/download/Aesthetic-Theme/Aesthetic.tar.gz
-sudo tar -xf Aesthetic.tar.gz --no-same-owner -C /boot/grub/themes/
-sudo echo "GRUB_THEME=/boot/grub/themes/Aesthetic/theme.txt" | sudo tee -a /etc/default/grub
 sudo echo "GRUB_DISABLE_OS_PROBER=false" | sudo tee -a /etc/default/grub
 sudo echo "GRUB_DEFAULT=saved" | sudo tee -a /etc/default/grub
 sudo echo "GRUB_SAVEDEFAULT=true" | sudo tee -a /etc/default/grub
 
 curl -fsSL https://trenek.github.io/grub_preconfig | sudo tee -a /etc/grub.d/40_custom
-
-sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
