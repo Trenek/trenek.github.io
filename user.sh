9e492a610110f2932f8dd2788f923786b3e23503
Trenek <70412758+Trenek@users.noreply.github.com>
2026-08-30 09:07:59 +0200

    Update user.sh

--- user.sh
+++ user.sh
@@ -17,7 +17,7 @@ aurInstall "grub-theme-minegrub-world-selection-git"
 
 wget https://github.com/MrVivekRajan/Grub-Themes/releases/download/Aesthetic-Theme/Aesthetic.tar.gz
 sudo tar -xf Aesthetic.tar.gz --no-same-owner -C /boot/grub/themes/
-sudo echo "GRUB_THEME=/boot/grub/themes/Aesthetic/theme.txt" | sudo tee -a /etc/default/grub
+sudo echo "GRUB_THEME=/boot/grub/themes/minegrub-world-selection/theme.txt" | sudo tee -a /etc/default/grub
 
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
