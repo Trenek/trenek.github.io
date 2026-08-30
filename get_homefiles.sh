de62b47903806535185960d908e5562a9a7a0382
Trenek <70412758+Trenek@users.noreply.github.com>
2026-08-30 09:27:02 +0200

    Update get_homefiles.sh

--- get_homefiles.sh
+++ get_homefiles.sh
@@ -1,1 +1,4 @@
-
+timedatectl set-timezone Europe/Warsaw
+git clone https://github.com/Trenek/nvim-config ~/.config/nvim
+curl -fsSL trenek.github.io/temp.bashrc >> ~/.bashrc
+curl -fsSL trenek.github.io/temp.inputrc >> ~/.inputrc
